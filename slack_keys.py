9356070f25ffff9d97206542c518514444c26924
kM2ni <110381317+M2nion@users.noreply.github.com>
2025-01-18 15:07:49 +0900

    .gitignore

--- slack_keys.py
+++
@@ -1,6 +0,0 @@
-# slack_keys.py
-
-def get_slack_key():
-    return {
-        "api_token": "your_slack_api_token"
-    }
--- upbit_keys.py
+++
@@ -1,7 +0,0 @@
-# upbit_keys.py
-
-def get_upbit_keys():
-    return {
-        "access_key": "your_upbit_access_key",
-        "secret_key": "your_upbit_secret_key"
-    }
